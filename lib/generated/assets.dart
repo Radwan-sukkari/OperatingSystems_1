a1babcdb5b939764f1c384f1991bb3b561f0ee24
RadwanSukkari <radwansukkari123@gmail.com>
2025-03-28 02:45:01 +0300

    radwan

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -4,11 +4,24 @@ class Assets {
 
   static const String fontsCairoRegular = 'assets/fonts/Cairo-Regular.ttf';
   static const String images1 = 'assets/images/1.jpg';
+  static const String images10 = 'assets/images/10.jpg';
+  static const String images11 = 'assets/images/11.jpg';
+  static const String images12 = 'assets/images/12.jpg';
+  static const String images13 = 'assets/images/13.jpg';
+  static const String images14 = 'assets/images/14.jpg';
+  static const String images15 = 'assets/images/15.jpg';
+  static const String images16 = 'assets/images/16.jpg';
+  static const String images17 = 'assets/images/17.jpg';
+  static const String images18 = 'assets/images/18.jpg';
+  static const String images19 = 'assets/images/19.jpg';
   static const String images2 = 'assets/images/2.jpg';
   static const String images3 = 'assets/images/3.jpg';
   static const String images4 = 'assets/images/4.jpg';
   static const String images5 = 'assets/images/5.jpg';
   static const String images6 = 'assets/images/6.jpg';
+  static const String images7 = 'assets/images/7.jpg';
+  static const String images8 = 'assets/images/8.jpg';
+  static const String images9 = 'assets/images/9.jpg';
   static const String imagesBoy = 'assets/images/boy.png';
   static const String imagesBoy6 = 'assets/images/boy 6.png';
   static const String imagesBoy10 = 'assets/images/boy10.png';
